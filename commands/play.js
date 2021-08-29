7643f515dd65be9e80d9d6a942e8e9c9a4d74f33
spitfires <akashfahad12@gmail.com>
2021-08-29 10:53:07 +0600

    h

--- commands/play.js
+++ commands/play.js
@@ -2,7 +2,7 @@ module.exports = {
       name: "play",
       code: `
       
-      $onlyIf[$message =!;You need to enter a song!]
+   
       $playSong[$message;5m;no;yes;could not play song!]
       $title[Added song to Queue]
       $description[Added $message to Queue]
